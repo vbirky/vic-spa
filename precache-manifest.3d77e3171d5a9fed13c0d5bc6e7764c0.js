2f5f9937526d928fa8bda8fd2c3c88db6b63bf47
Victor Birky <vic.birky@gmail.com>
2020-03-15 10:51:18 -0700

    Updates

---
+++ precache-manifest.3d77e3171d5a9fed13c0d5bc6e7764c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5cd99c0cc552d502edc9508fd15b5f",
+    "url": "/vic-spa/index.html"
+  },
+  {
+    "revision": "c9caa717c1d62e5e890c",
+    "url": "/vic-spa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ea1bbedb6b0378fb263",
+    "url": "/vic-spa/static/js/2.d0cfec9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vic-spa/static/js/2.d0cfec9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9caa717c1d62e5e890c",
+    "url": "/vic-spa/static/js/main.34c5c053.chunk.js"
+  },
+  {
+    "revision": "fb951f748a24f1aeaf2b",
+    "url": "/vic-spa/static/js/runtime-main.d89ddbe6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vic-spa/static/media/logo.5d5d9eef.svg"
+  }
+]);
